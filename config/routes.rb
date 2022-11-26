5cfbedad9b4a4bff3df69b143c9cbb11d6153e8d
Denis <mentalitapp@gmail.com>
2022-11-26 16:57:16 -0500

    added user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :pictures
+  
   get 'profile/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root 'pictures#new'
+
+  resources :users, shallow: true do
+    resources :pictures
+  end
+
 end
